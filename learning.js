7d498aaeb1ad7c827ada4f1da7164319783499ee
eminx <145885781+emi290@users.noreply.github.com>
2024-11-10 01:48:43 -0300

    Test 1

---
+++ learning.js
@@ -0,0 +1,11 @@
+alert ("Boas vindas ao site!");
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+let mensagemDeErro = "Erro! Preencha todos os campos";
+alert (mensagemDeErro);
+
+let nome = prompt ("Qual é o seu nome?");
+let idade = prompt ("Qual é a sua idade?");
+if(idade >= 18){"Você pode tirar sua habilitação!"}
+else{"Ahh!Você ainda não pode tirar habilitação!"}
